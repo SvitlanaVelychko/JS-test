9354ce5affe4e3b589076b5c28f3129ec463be7b
SvitlanaVelychko <svel2608@gmail.com>
2022-04-25 21:34:20 +0300

    test

--- js/test.js
+++ js/test.js
@@ -1,3 +1,37 @@
-const numbers = [1, 2, 3].concat([4, 5, 6], [7, 8, 9]);
+const atTheOldToad = {
+    potions: [
+        { name: "Speed potion", price: 460 },
+        { name: "Dragon breath", price: 780 },
+        { name: "Stone skin", price: 520 },
+    ],
+    // Change code below this line
+    getPotions() {
+        return this.potions;
+    },
+    addPotion(newPotion) {
+        if (this.potions.includes(newPotion)) {
+            return `Error! Potion ${newPotion} is already in your inventory!`;
+        }
 
-console.log(numbers);
+        this.potions.push(newPotion);
+    },
+    /*removePotion(potionName) {
+        const potionIndex = this.potions.indexOf(potionName);
+
+        if (potionIndex === -1) {
+            return `Potion ${potionName} is not in inventory!`;
+        }
+
+        this.potions.splice(potionIndex, 1);
+    },
+    updatePotionName(oldName, newName) {
+        const potionIndex = this.potions.indexOf(oldName);
+
+        if (potionIndex === -1) {
+            return `Potion ${oldName} is not in inventory!`;
+        }
+
+        this.potions.splice(potionIndex, 1, newName);
+    },
+    // Change code above this line*/
+};
