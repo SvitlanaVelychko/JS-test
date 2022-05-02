37b5ddb23d31d0ab14667593432781c9048d7d57
SvitlanaVelychko <svel2608@gmail.com>
2022-05-02 22:03:51 +0300

    test

--- js/test.js
+++ js/test.js
@@ -1,29 +1,96 @@
-function processCall(recipient, onAvailable, onNotAvailable) {
-  // Імітуємо доступність абонента випадковим числом
-  const isRecipientAvailable = Math.random() > 0.5;
+// const filter = (array, test) => {
+//   const filteredArray = [];
 
-  if (!isRecipientAvailable) {
-    onNotAvailable(recipient);
-    return;
-  }
+//   for (const el of array) {
+//     console.log(el);
+//     const passed = test(el);
 
-  onAvailable(recipient);
+//     if (passed) {
+//       filteredArray.push(el);
+//     }
+//   }
+
+//   return filteredArray;
+// }
+
+
+// console.log(filter([1, 2, 3, 4, 5], value => value >= 3));
+// console.log(filter([1, 2, 3, 4, 5, 6, 7, 8, 9], value => value < 5));
+
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true, },
+//   { name: 'grapes', quantity: 150, isFresh: false, },
+//   { name: 'bananas', quantity: 100, isFresh: true, },
+// ];
+
+
+// console.log(filter(fruits, fruit => fruit.quantity >= 120));
+
+// const fnA = function (parametr) {
+//   const innerVeriable = 'Это вызывает функцию fnA';
+
+//   const innerFunction = function () {
+//     console.log(parametr);
+//     console.log(innerVeriable);
+//     console.log('Это вызов innerFunction');
+//   };
+
+//   return innerFunction;
+// }
+
+// const fnB = fnA(555);
+
+// fnB();
+
+// console.log(fnB);
+
+// const makeDish = function (sheffName, dish) {
+//   console.log(`${sheffName} готовит ${dish}`);
+// }
+
+// makeDish('Mango', 'чай');
+// makeDish('Mango', 'суп');
+// makeDish('Mango', 'котлеты');
+
+// const makeSheff = function (name) {
+//   const makeDish = function (dish) {
+//     console.log(`${name} готовит ${dish}`);
+//   };
+
+//   return makeDish;
+// }
+
+// const mango = makeSheff('Mango');
+// console.dir(mango);
+
+// mango('чай');
+// mango('суп');
+// mango('котлеты');
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   firstArray.forEach(function(number) {
+//     if (secondArray.includes(number)) {
+//       commonElements.push(number);
+//     }
+//   });
+
+//   return commonElements;
+//   // Change code above this line
+// }
+
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
 }
 
-function takeCall(name) {
-  console.log(`З'єднуємо з ${name}, очікуйте...`);
-  // Логіка прийняття дзвінка
-}
-
-function activateAnsweringMachine(name) {
-  console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
-  // Логіка активації автовідповідача
-}
-
-function leaveHoloMessage(name) {
-  console.log(`Абонент ${name} недоступний, записуємо голограму.`);
-  // Логіка запису голограми
-}
-
-processCall("Манго", takeCall, activateAnsweringMachine);
-processCall("Полі", takeCall, leaveHoloMessage);
+console.log(calculateTotalPrice([12, 85, 37, 4]));
