7de7410c7c452a64f8608fe7a188648a1bf20b43
SvitlanaVelychko <svel2608@gmail.com>
2022-05-13 20:19:56 +0300

    test

--- js/test.js
+++ js/test.js
@@ -1,4 +1,6 @@
 class User {
+  email;
+
   constructor(email) {
     this.email = email;
   }
@@ -11,12 +13,40 @@ class User {
     this.email = newEmail;
   }
 }
-// Change code below this line
 class Admin extends User {
+  // Change code below this line
+
   static AccessLevel = {
     BASIC: "basic",
     SUPERUSER: "superuser",
   };
+
+  constructor({ email, accessLevel, blacklistedEmails = []}) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = blacklistedEmails;
+  }
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return (this.blacklistedEmails.includes(email));
+  }
+
+  // Change code above this line
 }
 
-console.log(Admin.AccessLevel.BASIC);
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
