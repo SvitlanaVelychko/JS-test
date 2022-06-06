358abdab5a89e9db4a56bc6e81f4c09fa27ae70a
SvitlanaVelychko <svel2608@gmail.com>
2022-06-06 12:09:44 +0300

    test

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const totalCategories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${totalCategories.length}`);
-
-const itemEl = [...totalCategories]
-.map(category => `Category: ${category.firstElementChild.textContent}
-Elements: ${category.lastElementChild.children.length}`
-).
-join('\r\n');
-console.log(itemEl);
